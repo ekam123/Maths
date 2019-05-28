994ca2b6c4e1e2e3952c528810ba2b4edd76aa15
Ekam Singh Dhaliwal <ekamsingh123.es@gmail.com>
2019-05-28 12:43:02 -0700

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Ekam Singh Dhaliwal on 2019-05-28.
+//  Copyright © 2019 ekam-singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
